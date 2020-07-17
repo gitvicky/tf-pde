e91b891a5b07f7663db2dad861a0d02f07ec80eb
gitvicky <vignesh7g@gmail.com>
2020-07-17 19:52:42 +0100

    Pip glitches

--- setup.py
+++ setup.py
@@ -8,24 +8,26 @@ Created on Fri Jul 10 19:23:09 2020
 Python Package Setup
 """
 import io
+from os import path
 from setuptools import setup
 from setuptools import find_packages
 
 
-with io.open("README.md", "r", encoding="utf-8") as f:
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_desc = f.read()
 
-with open("requirements.txt", "r") as f:
-    install_requires = [x.strip() for x in f.readlines()]
-
-
+with open(path.join(this_directory, 'requirements.txt'), encoding='utf-8') as f:
+    install_requires = f.read()
+    
 setup(
-    name='Neural_PDE',
-    version='0.1dev',
+    name='tf-pde',
+    version='0.2dev',
     description="Deep learning library for solving partial differential equations",
     author="Vignesh Gopakumar",
-    author_email="vignesh7g@gmail.com@gmail.com",
-    url = "https://github.com/gitvicky/Neural_PDE",
+    author_email="vignesh7g@gmail.com",
+    url = "https://github.com/gitvicky/tf-pde",
     packages = find_packages(),
     license='MIT',
     long_description = long_desc,
